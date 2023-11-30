83e55be94818d16cc01804917fb48f2a650ddd40
MaiUserIsMai <145808207+MaiUserIsMai@users.noreply.github.com>
2023-11-29 21:11:35 -0800

    Update myAccount.js

--- public_html/Account/myAccount.js
+++ public_html/Account/myAccount.js
@@ -4,14 +4,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
   tabs.forEach((tab, index) => {
     tab.addEventListener("click", () => {
+      // Hide all tab contents
       tabContents.forEach((content) => {
         content.classList.remove("active");
       });
+
+      // Show the clicked tab content
+      tabContents[index].classList.add("active");
+
+      // Remove 'active' class from all tabs
       tabs.forEach((tab) => {
         tab.classList.remove("active");
       });
-      tabContents[index].classList.add("active");
-      tabs[index].classList.add("active");
+
+      // Add 'active' class to the clicked tab
+      tab.classList.add("active");
     });
   });
 });
