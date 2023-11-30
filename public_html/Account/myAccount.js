cdc3ef86e163bbdd76f3b44bbe46fd4345deef0b
MaiUserIsMai <145808207+MaiUserIsMai@users.noreply.github.com>
2023-11-29 22:04:32 -0800

    Update myAccount.js

--- public_html/Account/myAccount.js
+++ public_html/Account/myAccount.js
@@ -1,8 +1,14 @@
- function openTab(tabName) {
+function openTab(tabName) {
         var tabs = document.querySelectorAll('.tab-content');
         tabs.forEach(function(tab) {
             tab.style.display = 'none';
         });
 
+        var tabButtons = document.querySelectorAll('.tab');
+        tabButtons.forEach(function(button) {
+            button.classList.remove('active');
+        });
+
         document.getElementById(tabName).style.display = 'grid';
+        event.currentTarget.classList.add('active');
     }
