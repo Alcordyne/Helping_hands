ae9cd93a934b6090acc23308b04749c2ea484792
Alcordyne <145807337+Alcordyne@users.noreply.github.com>
2023-11-29 17:57:03 -0800

    Update search.js

--- public_html/Function/search.js
+++ public_html/Function/search.js
@@ -2,10 +2,10 @@
 
 var pages = [
   { title: "Student Homelessness Certification Form", content: "Fee waiver for a High School Equivalency Test", link: "../FAQ/HomelessForm.html" },
-  { title: "Disability Certification Form (SSF)", content: "Uused only for the purpose of qualifying for housing assistance ", link: "../FAQ/DisForm.html" },
+  { title: "Disability Certification Form (SSF)", content: "Used only for the purpose of qualifying for housing assistance ", link: "../FAQ/DisForm.html" },
   { title: "Birth Certificate for Homelessness", content: "Fee-exempt copy of a birth record", link: "../FAQ/BirthForm.html" },
   { title: "P.O. Box Form (Form 1093)", content: "For individuals who are in need of a P.O. Box (Post Office Box).", link: "../FAQ/POForm.html" },
-  { title: "Guides", content: "Frequently asked questions and guides", link: "../FAQ/FAQGuides.html" },
+  { title: "Guides", content: "FAQ / Frequently asked questions and guides", link: "../FAQ/FAQGuides.html" },
   { title: "Job Assistance", content: "Resume support and the best practices to get a job.", link: "../Job/jobAssistance.html" },
 ];
 
