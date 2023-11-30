cbd04cf1ace0a4096f85c60505152db66db05eb9
MaiUserIsMai <145808207+MaiUserIsMai@users.noreply.github.com>
2023-11-30 09:36:39 -0800

    Update stateswitch.js

--- public_html/Function/stateswitch.js
+++ public_html/Function/stateswitch.js
@@ -1,25 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const  = document.getElementById('loginButton');
-    const  = document.getElementById('accountButton');
+    const signInButton = document.getElementById('signinButton');
+    const MyAccount = document.getElementById('accountButton');
 
     // Check user login status using a session cookie or other authentication mechanism
     const userLoggedIn = checkUserLoginStatus(); // Implement this function
 
     if (userLoggedIn) {
-        loginButton.style.display = 'none';
+        signinButton.style.display = 'none';
         accountButton.style.display = 'block';
 
         accountButton.addEventListener('click', () => {
             // Redirect to the user account page
-            window.location.href = 'account.html';
+            window.location.href = 'myAccount.html';
         });
     } else {
-        loginButton.style.display = 'block';
+        signinButton.style.display = 'block';
         accountButton.style.display = 'none';
 
-        loginButton.addEventListener('click', () => {
+        signinButton.addEventListener('click', () => {
             // Redirect to the login page
-            window.location.href = 'login.html';
+            window.location.href = '.html';
         });
     }
 });
