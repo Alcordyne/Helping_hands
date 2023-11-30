e358df6c9d4493610f29ecefbd1e6976a73260d9
MaiUserIsMai <145808207+MaiUserIsMai@users.noreply.github.com>
2023-11-29 20:59:30 -0800

    Update myAccount.js

--- public_html/Account/myAccount.js
+++ public_html/Account/myAccount.js
@@ -1,14 +1,17 @@
-let tabs = document.querySelectorAll(".tabs h3");
-let tabContents = document.querySelectorAll(".tab-content div");
-tabs.forEach((tab, index) => {
-  tab.addEventListener("click", () => {
-    tabContents.forEach((content) => {
-      content.classList.remove("active");
+document.addEventListener("DOMContentLoaded", function () {
+  let tabs = document.querySelectorAll(".tabs h3");
+  let tabContents = document.querySelectorAll(".tab-content div");
+
+  tabs.forEach((tab, index) => {
+    tab.addEventListener("click", () => {
+      tabContents.forEach((content) => {
+        content.classList.remove("active");
+      });
+      tabs.forEach((tab) => {
+        tab.classList.remove("active");
+      });
+      tabContents[index].classList.add("active");
+      tabs[index].classList.add("active");
     });
-    tabs.forEach((tab) => {
-      tab.classList.remove("active");
-    });
-    tabContents[index].classList.add("active");
-    tabs[index].classList.add("active");
   });
 });
